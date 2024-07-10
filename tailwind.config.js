6578e4885c5b11f9f136d8ffa48323631d9bbc2e
0xmimiQ <0xmimiq@gmail.com>
2024-07-11 00:27:16 +0800

    Adjust linkcard animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,16 @@ export default {
         wave: "wave 3s linear infinite",
         "wave-slow": "wave 5s linear infinite",
         "wave-slower": "wave 8s linear infinite",
+        "lower-bounce": "lowerBounce 2s ease-in-out infinite",
       },
       keyframes: {
         wave: {
           "0%": { transform: "translateX(0)" },
           "100%": { transform: "translateX(-50%)" },
+        },
+        lowerBounce: {
+          "0%, 100%": { transform: "translateY(-8%); opacity: 1" },
+          "50%": { transform: "translateY(0%); opacity: .7" },
         },
       },
       fontFamily: {
