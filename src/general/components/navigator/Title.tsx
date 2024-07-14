a269c8a06e8e3f85897dedab46b8b1430cb3d9de
0xmimiQ <0xmimiq@gmail.com>
2024-07-14 22:56:33 +0800

    adjust title layout

--- src/general/components/navigator/Title.tsx
+++ src/general/components/navigator/Title.tsx
@@ -6,7 +6,7 @@ function Title() {
     <NavLink
       id="title"
       to="/"
-      className="font-orbitron flex select-none flex-row text-3xl text-gray-500 transition-all duration-500 dark:text-white xl:mr-40 xl:text-black"
+      className="font-orbitron flex select-none flex-row items-center text-3xl text-gray-500 transition-all duration-500 dark:text-white xl:mr-40 xl:text-black"
     >
       <img src={Logo} alt="logo" className="mr-2 w-12" />
       PolyME
