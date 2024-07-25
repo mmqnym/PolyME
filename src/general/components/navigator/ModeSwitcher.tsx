7854f611de211bfcf92f7b0976dcd8f14dc6b39c
0xmimiQ <0xmimiq@gmail.com>
2024-07-25 19:28:19 +0800

    Improve performance and record custom settings

--- src/general/components/navigator/ModeSwitcher.tsx
+++ src/general/components/navigator/ModeSwitcher.tsx
@@ -14,8 +14,10 @@ function ModeSwitcher() {
   useEffect(() => {
     if (navigatorContext.isDarkMode) {
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "normal");
     }
   }, [navigatorContext.isDarkMode]);
 
--- src/general/components/navigator/Title.tsx
+++ src/general/components/navigator/Title.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.webp";
+import SuspenseImage from "../lazyLoader/SuspenseImage";
 
 function Title() {
   return (
@@ -8,7 +9,7 @@ function Title() {
       to="/"
       className="flex select-none flex-row items-center font-orbitron text-3xl text-gray-500 transition-all duration-500 dark:text-white xl:mr-[16%] xl:text-black"
     >
-      <img src={Logo} alt="logo" className="mr-2 w-12" />
+      <SuspenseImage src={Logo} alt="logo" className="mr-2 w-12" />
       PolyME
     </NavLink>
   );
