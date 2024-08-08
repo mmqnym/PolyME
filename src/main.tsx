d1b2039811b8804374564e6f9f6ba250768b4e2d
0xmimiQ <0xmimiq@gmail.com>
2024-08-08 10:54:35 +0800

    test migration service

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<>
 		<App />
-		<Analytics />
-		<SpeedInsights />
+		{/* Any analytics or other scripts here */}
 	</>,
 );
