410f17aa236b544a8af2e7f228d7f6c0149b3071
0xmimiQ <0xmimiq@gmail.com>
2024-08-08 17:07:53 +0800

    support gtm

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<>
-		<App />
-		{/* Any analytics or other scripts here */}
-	</>,
-);
+import TagManager from "react-gtm-module";
+
+TagManager.initialize({ gtmId: "GTM-TKLQL6PP" });
+
+// biome-ignore lint/style/noNonNullAssertion: must have root
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
