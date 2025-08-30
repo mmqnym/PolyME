4e1daec36b00fc97675b3ce8d182737351ecd8cc
mmqnym <mail@mmq.dev>
2025-08-31 01:26:17 +0800

    refact(languageSwitcher): apply animation when hover and leave

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,63 +2,73 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
-	theme: {
-		screens: {
-			sm: "640px",
-			md: "768px",
-			lg: "1024px",
-			xl: "1280px",
-			"2k": "2560px",
-		},
-		extend: {
-			animation: {
-				wave: "wave 8s linear infinite",
-				"lower-bounce": "lowerBounce 1s ease-in-out infinite",
-				ripple: "ripple 1.5s cubic-bezier(0, 0.2, 0.8, 1) infinite",
-				"slow-jump": "slowJump 5s ease-in-out infinite",
-			},
-			keyframes: {
-				wave: {
-					"0%": { transform: "translateX(0)" },
-					"100%": { transform: "translateX(-50%)" },
-				},
-				lowerBounce: {
-					"0%, 100%": {
-						transform: "translateY(-10%); opacity: 1",
-						animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
-					},
-					"50%": {
-						transform: "none; opacity: .8",
-						animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
-					},
-				},
-				ripple: {
-					"0%": { transform: "scale(0.8)", opacity: 1 },
-					"100%": { transform: "scale(2.4)", opacity: 0 },
-				},
-				slowJump: {
-					"0%": { transform: "translateY(0)" },
-					"50%": { transform: "translateY(-1%)" },
-					"100%": { transform: "translateY(0)" },
-				},
-			},
-			fontFamily: {
-				orbitron: ["Orbitron", "sans-serif"],
-				ubuntu: ["Ubuntu", "sans-serif"],
-				iceland: ["Iceland", "sans-serif"],
-				kosugimaru: ["Kosugi Maru", "sans-serif"],
-				yupearl: ["yupearl", "sans-serif"],
-				chillroundh2othic: ["chillroundh2othic", "sans-serif"],
-			},
-		},
-	},
-	plugins: [
-		require("tailwind-scrollbar")({
-			nocompatible: true,
-			preferredStrategy: "pseudoelements",
-		}),
-		flowbite.plugin(),
-	],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2k": "2560px",
+    },
+    extend: {
+      animation: {
+        wave: "wave 8s linear infinite",
+        "lower-bounce": "lowerBounce 1s ease-in-out infinite",
+        ripple: "ripple 1.5s cubic-bezier(0, 0.2, 0.8, 1) infinite",
+        "slow-jump": "slowJump 5s ease-in-out infinite",
+        fade: "fade 0.8s ease-in-out forwards",
+        "fade-back": "fade-back 0.8s ease-in-out forwards",
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+        lowerBounce: {
+          "0%, 100%": {
+            transform: "translateY(-10%); opacity: 1",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "none; opacity: .8",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+        ripple: {
+          "0%": { transform: "scale(0.8)", opacity: 1 },
+          "100%": { transform: "scale(2.4)", opacity: 0 },
+        },
+        slowJump: {
+          "0%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-1%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fade: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        "fade-back": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+      },
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+        iceland: ["Iceland", "sans-serif"],
+        kosugimaru: ["Kosugi Maru", "sans-serif"],
+        yupearl: ["yupearl", "sans-serif"],
+        chillroundh2othic: ["chillroundh2othic", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("tailwind-scrollbar")({
+      nocompatible: true,
+      preferredStrategy: "pseudoelements",
+    }),
+    flowbite.plugin(),
+  ],
 };
